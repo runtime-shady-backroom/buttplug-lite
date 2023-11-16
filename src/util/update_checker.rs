1e49fd0821daf46ef0442b00d81a569f7e44fc0f
Runtime's Shady Backroom <76618880+runtime-shady-backroom@users.noreply.github.com>
2023-11-16 15:33:56 -0600

    Switch from hyper to reqwest

--- src/util/update_checker.rs
+++ src/util/update_checker.rs
@@ -7,10 +7,6 @@
 use std::cmp::Ordering;
 use std::time::Duration;
 
-use bytes::Buf as _;
-use hyper::{self, Body, Client, Method, Request, Response, Uri};
-use hyper_timeout::TimeoutConnector;
-use hyper_tls::HttpsConnector;
 use semver::Version;
 use serde::Deserialize;
 use tracing::{info, warn};
@@ -60,36 +56,23 @@ pub async fn check_for_update(local_version: Version) -> Option<String> {
 
 /// Get latest release from GitHub
 async fn get_latest_release() -> Result<GithubRelease, String> {
-    let uri: Uri = UPDATE_CHECK_URI.parse().map_err(|e| format!("update check URI failed to parse: {e:?}"))?;
-    let connector = HttpsConnector::new();
-    let mut connector = TimeoutConnector::new(connector);
+    let client = reqwest::Client::builder()
+        .connect_timeout(Duration::from_secs(10))
+        .timeout(Duration::from_secs(3))
+        .build()
+        .map_err(|e| format!("Failed to build HTTP client: {e}"))?;
 
-    // We wait for this to complete before we open the GUI, so we MUST have a short timeout.
-    connector.set_connect_timeout(Some(Duration::from_secs(10)));
-    connector.set_read_timeout(Some(Duration::from_secs(3)));
-    connector.set_write_timeout(Some(Duration::from_secs(3)));
+    let request = client.get(UPDATE_CHECK_URI)
+        .header(reqwest::header::USER_AGENT, USER_AGENT)
+        .build()
+        .map_err(|e| format!("Failed to build update check HTTP request: {e}"))?;
 
-    let client = Client::builder()
-        .build::<_, Body>(connector);
+    let response = client.execute(request).await
+        .map_err(|e| format!("Update check failed: {e}"))?;
 
-    let request = Request::builder()
-        .method(Method::GET)
-        .uri(uri)
-        .header(hyper::header::USER_AGENT, USER_AGENT)
-        .body(Body::empty())
-        .expect("failed to build github release request");
-
-    let response: Response<Body> = client.request(request).await
-        .map_err(|e| format!("Could not read github release response body: {e:?}"))?;
-    deserialize_response(response).await
-}
-
-/// deserialize a GitHub API response
-async fn deserialize_response(response: Response<Body>) -> Result<GithubRelease, String> {
-    let body = hyper::body::aggregate(response).await
-        .map_err(|e| format!("error aggregating github release response body: {e:?}"))?;
-    serde_json::from_reader(body.reader())
-        .map_err(|e| format!("error parsing github release response body: {e:?}"))
+    // note that this buffers the entire JSON response before it begins parsing
+    response.json::<GithubRelease>().await
+        .map_err(|e| format!("error parsing github release response body: {e}"))
 }
 
 /// GitHub API response object
